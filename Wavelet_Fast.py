e52a136d8c0a349c113ad6f59269e8f6f807e647
Aria Hemmatian <AriaHemmatian81@gmail.com>
2024-10-30 19:33:49 +0330

    Added comments and further cleaned up the code

--- Wavelet_Fast.py
+++ Wavelet_Fast.py
@@ -15,49 +15,48 @@ from scipy import stats
 #-----------------------------------------------------------------
 #---------------------- Pre-Processing ---------------------------
 #-----------------------------------------------------------------
+
 path = str(pathlib.Path(__file__).parent.resolve())
 
 def preprocess(name,filename,crop_cor = []):
-    #------------------------------
-    #-- Retrieving observational data
-    #------------------------------
+    # Retrieves observational data
     outname = 'Output/'+name+'/'+name+'_'
     adata = fits.open(path+'\\data\\'+filename)
     a = adata[0].data
-    #------------------------------
-    #-- Cropping and zero padding
-    #------------------------------
+    
+    # Crop the image if it's required
     if crop_cor != []:
         a=a[crop_cor[0][0]:crop_cor[0][1],crop_cor[1][0]:crop_cor[1][1]]
 
     kont = np.array(a.astype(float))
     kont = kont/np.max(kont)  # Normalize to 1
 
-    plt.imshow(kont)
+    # Plot and save the image of the object
+    plt.imshow(kont, origin='lower', interpolation='nearest')
     plt.colorbar()
     plt.savefig(path+"\\"+outname +'original.png', dpi=200)
     #plt.show()
     plt.clf()
 
+    # Zero-padding the image to a square to reduce the complications with Pet Hat later on
     ny = kont[0,:].size
     nx = kont[:,0].size
     kont = np.pad(kont,[(0,int(np.max([-nx+ny,0]))), (0,int(np.max([nx-ny,0])))], mode='constant') 
 
-    return kont, nx, ny # Returns the final map and the save path
+    return kont, nx, ny # Returns the final map and the shape of the original image
 
 #-----------------------------------------------------------------
 #---------------------- The Pet-Hat function ---------------------
 #-----------------------------------------------------------------
 
-
-def pethat_k(k):
+def pethat_k(k): # This function is in the fourier space 
     if ((np.pi <= k)and(k <= (4.0*np.pi))): 
         return np.cos(np.pi * 0.5 * np.log2(k * 0.5/np.pi))**2
     else:
         return 0.
 
 
-def pethat_phi_func(data,a_scale):
+def pethat_phi_func(data,a_scale): # Builds an array of a 2d Pet Hat map using scale
     ny = data[0,:].size
     nx = data[:,0].size
     val_map = np.zeros((nx, ny), np.float32)
@@ -71,19 +70,24 @@ def pethat_phi_func(data,a_scale):
 #-----------------------------------------------------------------
 
 
-def pethat_wavelet_scale_analysis(kont, nx, ny, n = 100):
+def pethat_wavelet_scale_analysis(kont, nx, ny, start=2, steps=1, n = 100):
 
+    # Produces a complete FFT of the image
     fft_shape = 2*np.array(kont.shape) - 1
     fft_kont = scipy.fft.fftshift(scipy.fft.fft2(kont,fft_shape))
+
+    # Calculates the coefficient needed to convert the unit of the sacle to pixels
     fnx = fft_kont[:,0].size
     coeff = 2*np.pi/fnx 
 
-    en_scales = 2 + np.arange(0,n) / n * 100
+    # Defines the array for scales and wavelet coefficients
+    en_scales = start + np.arange(0,n) * steps
     wavelet_coeffs = np.clongdouble(np.zeros((n,nx,ny)))
 
+    # Calculates the wavelet coefficients for all the scales
     with alive_bar(n) as bar: 
         for i in range(n):
-            a = en_scales[i] * coeff
+            a = en_scales[i] * coeff # The scale
             map1 = scipy.fft.ifft2(scipy.fft.ifftshift(fft_kont*pethat_phi_func(fft_kont,a)))
             map1 = map1[0:nx,0:ny]
             wavelet_coeffs[i] = map1
@@ -97,6 +101,16 @@ def pethat_wavelet_scale_analysis(kont, nx, ny, n = 100):
 
       
 class wavelet_results:
+    """ 
+    This object consists of the name of the observed object, the cube of wavelet coefficients, and all the corresponding scales. 
+    The object can then produce all the needed data, utilizing several functions. 
+    The possible (at least for now) actions consist of:
+    creating a gif from the data cube of wavelet coeffs, 
+    saving the data cube in a FITS file, 
+    saving the wavelet coeffs as PNG images, 
+    calculating the energies and plot the energy over scale plot, 
+    and calculating the sum of the wavelet coeffs. 
+    """
     def __init__(self, cube, scales , name):
         self.cube = cube
         self.scales = scales
@@ -162,11 +176,10 @@ class wavelet_results:
 
 
 #-----------------------------------------------------------------
-#-------------------- Wavelet Scale Correlation ------------------
+#-------------------- Wavelet Scale Cross-Correlation ------------------
 #-----------------------------------------------------------------
 
-
-def give_names(str_a,str_b):
+def give_names(str_a,str_b): # This function extracts the name of the object and the filters for the cross-correlation function
     name = ''
     for i in range(len(str_a)):
         if str_a[i] == '_':
@@ -176,14 +189,16 @@ def give_names(str_a,str_b):
         name+=(str_a[i])  
     return name , str_a, str_b
 
-def plot_correlation(cube_a, cube_b):
+def plot_correlation(cube_a, cube_b): # This function gets two result objects and if they are compatible, plots their scale cross-correlation over scale
     energies_a = cube_a.calc_energies(do_plot=False)
     energies_b = cube_b.calc_energies(do_plot=False)
     if (not np.allclose(cube_a.scales,cube_b.scales)) or np.shape(cube_a.cube) != np.shape(cube_b.cube):
         print('Error: Cubes are not compatible.')
         return
     (n,nx,ny) = np.shape(cube_a.cube)
-    corr = np.zeros(n) #Calculating the cross-correlation and its error
+    
+    # Calculating the cross-correlation and its error
+    corr = np.zeros(n)
     for i in range(n):
         corr[i] = np.real(np.sum(cube_a.cube[i]*np.conjugate(cube_b.cube[i]))/np.sqrt(energies_a[i]*energies_b[i]))
     corr_err = np.sqrt(1 - corr**2)/ np.sqrt(nx*ny/cube_a.scales**2 - 2)
@@ -194,6 +209,6 @@ def plot_correlation(cube_a, cube_b):
     plt.xlabel(r'Scale (pixels)', fontsize=14)
     plt.ylabel(r'Correlation', fontsize=14)
     plt.title(r'Scale Correlation of '+obj_name+' in '+filt_a+' and '+filt_b+' Filters', fontsize=16)
-    plt.savefig(path+'\\'+'Output/'+cube_a.name+'_'+cube_b.name+'_pethat_corr_smooth.png', dpi=400)
+    plt.savefig(path+'\\'+'Output/'+obj_name+'_'+filt_a+'_'+filt_b+'_pethat_corr_smooth.png', dpi=400)
     plt.show()
     return
